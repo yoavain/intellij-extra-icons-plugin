c4113a9ee38c93fca9e3f54bcad8c0276ea57f89
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2019-03-29 15:33:21 +0100

    refactoring

---
+++ src/lermitage/intellij/extra/icons/BaseIconProvider.java
@@ -0,0 +1,67 @@
+package lermitage.intellij.extra.icons;
+
+import com.intellij.ide.IconProvider;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import lermitage.intellij.extra.icons.cfg.SettingsService;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.Icon;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Edoardo Luppi
+ */
+public abstract class BaseIconProvider extends IconProvider {
+    
+    private List<Model> models;
+    
+    /**
+     * Returns the Model(s) supported by this icon provider.
+     * This method will be called exactly once, so there is no need
+     * to provide a static array, as the array returned by this method is cached.
+     */
+    @NotNull
+    protected abstract List<Model> getModels();
+    
+    /**
+     * Check whether this icon provider supports the input file.
+     */
+    protected abstract boolean isSupported(@NotNull final PsiFile psiFile);
+    
+    @Nullable
+    @Override
+    public final Icon getIcon(@NotNull final PsiElement psiElement, final int flags) {
+        final Optional<PsiFile> optFile = Optional.ofNullable(psiElement.getContainingFile());
+        final PsiFile file;
+        
+        if (optFile.isPresent() && isSupported(file = optFile.get())) {
+            final String fileName = file.getName().toLowerCase();
+            final List<Model> models = lazyGetModels();
+            
+            for (final Model model : models) {
+                if (model.check(fileName)) {
+                    return IconLoader.getIcon(model.getIcon());
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Lazily computes the Model(s) used by this icon provider.
+     * This should be already thread safe in the IDEA platform context.
+     */
+    private List<Model> lazyGetModels() {
+        if (models == null) {
+            models = getModels();
+            List<String> disabledModelIds = SettingsService.getDisabledModelIds();
+            models.forEach(model -> model.setEnabled(!disabledModelIds.contains(model.getId())));
+        }
+        return models;
+    }
+}
